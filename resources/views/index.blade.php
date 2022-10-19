ec0fd9d41732bfd254384705aac19bd0a6160842
adelano <adelano@softwar.com.br>
2022-10-19 09:17:23 -0300

    start projet

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>    
+      
+        
+    </head>
+    <body> 
+        
+        <app-root></app-root>
+        <script src="{{ asset('js/runtime.e52a78f68d3a501f.js') }}" type="module"></script>
+        <script src="{{ asset('js/polyfills.4a877794f0c37d8b.js') }}" type="module"></script>
+        <script src="{{ asset('js/main.8c410ee68ca9c791.js') }}" type="module"></script>
+
+    </body>
+</html>
