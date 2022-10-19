b0010b15ea1e486ac4b877eabcacad8ebb0bcd75
adelano <adelano@softwar.com.br>
2022-10-19 11:15:38 -0300

    instalação do Angular material

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
         <title>Laravel</title>    
       
@@ -13,7 +14,7 @@
         <app-root></app-root>
         <script src="{{ asset('js/runtime.e52a78f68d3a501f.js') }}" type="module"></script>
         <script src="{{ asset('js/polyfills.4a877794f0c37d8b.js') }}" type="module"></script>
-        <script src="{{ asset('js/main.8c410ee68ca9c791.js') }}" type="module"></script>
+        <script src="{{ asset('js/main.6c44c74c8a00b8e0.js') }}" type="module"></script>
 
     </body>
 </html>
