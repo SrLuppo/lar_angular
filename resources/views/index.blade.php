ce0c24e560819e425c5f411d0b2345d98eee772c
adelano <adelano@softwar.com.br>
2022-10-26 13:35:29 -0300

    211

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,7 +14,7 @@
         <app-root></app-root>
         <script src="{{ asset('js/runtime.e52a78f68d3a501f.js') }}" type="module"></script>
         <script src="{{ asset('js/polyfills.4a877794f0c37d8b.js') }}" type="module"></script>
-        <script src="{{ asset('js/main.6c44c74c8a00b8e0.js') }}" type="module"></script>
+        <script src="{{ asset('js/main.2e1a67645f02a718.js') }}" type="module"></script>
 
     </body>
 </html>
